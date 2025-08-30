e53df4ea48997f2bdd3e2df334034d0c7f839a20
James Smith <{ID}+{username}@users.noreply.github.com>
2025-08-30 09:04:37 -0600

    183: Reduce potential frequent reallocations

--- src/hostLib/tty_parsers/SerialStreamParser.cpp
+++ src/hostLib/tty_parsers/SerialStreamParser.cpp
@@ -227,7 +227,13 @@ void SerialStreamParser::process()
 
             mEndMarkers.pop_front();
             mParserRx.erase(mParserRx.begin(), eol + 1);
-            mParserRx.shrink_to_fit();
+
+            // Only shrink if capacity is much larger than size
+            if (mParserRx.capacity() >= (2 * mParserRx.size()))
+            {
+                mParserRx.shrink_to_fit();
+            }
+
             for (std::size_t& s : mEndMarkers)
             {
                 s -= (pos + 1);
